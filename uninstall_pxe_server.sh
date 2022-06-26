de58b8ec5fdf6a0a88651d133196e5d48c47fcf7
M-Paladin <49365410+M-Paladin@users.noreply.github.com>
2022-06-26 21:13:56 +0300

    Create uninstall_pxe_server.sh

---
+++ uninstall_pxe_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installer curl pour lancer ce script
+# sudo apt install curl
+
+# puis pour l'exécuter
+# curl https://raw.githubusercontent.com/M-Paladin/lvsb/main/uninstall_pxe_server.sh | bash
+
+# Suppression de dnsmasq (DHCP, TFTP)
+sudo apt remove --purge dnsmasq
