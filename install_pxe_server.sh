0ad6b5029d6bf3e336e750ccec7cddbef69f7955
M-Paladin <49365410+M-Paladin@users.noreply.github.com>
2022-06-26 21:58:12 +0300

    Update install_pxe_server.sh

--- install_pxe_server.sh
+++ install_pxe_server.sh
@@ -3,13 +3,14 @@
 # sudo apt install curl
 
 # puis pour l'exécuter
-# curl curl -H 'Authorization: token GHSAT0AAAAAABWAJ3QCTMCTROBMCJK567PWYVYULVQ' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/M-Paladin/lvsb/main/install_pxe_server.sh | bash
+# https://raw.githubusercontent.com/M-Paladin/lvsb/main/install_pxe_server.sh | bash
 
 
-# Setup Debian PXEinstall environment
+# Configuration d'un serveur Debian PXE
 
 # Mise à jour des sources Debian
 sudo apt update
 
 # Installation de dnsmasq (DHCP, TFTP)
 sudo apt install dnsmasq
+
