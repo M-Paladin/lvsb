a3f13288cc65c642a871cb464c55e0f17bd2d58a
M-Paladin <49365410+M-Paladin@users.noreply.github.com>
2022-06-26 22:39:38 +0300

    Update install_pxe_server.sh

--- install_pxe_server.sh
+++ install_pxe_server.sh
@@ -5,8 +5,10 @@
 # puis pour l'exécuter
 # https://raw.githubusercontent.com/M-Paladin/lvsb/main/install_pxe_server.sh | bash
 
-
 # Configuration d'un serveur Debian PXE
+
+RESEAU="192.168.1.1"
+DHCP-SERVEUR="192.168.1.1"
 
 # Mise à jour des sources Debian
 sudo apt-get update
@@ -14,3 +16,17 @@ sudo apt-get update
 # Installation de dnsmasq (DHCP, TFTP)
 sudo apt-get install -y dnsmasq
 
+# Configuration de dnsmasq
+echo "
+port=0
+dhcp-range=$RESEAU,proxy
+dhcp-boot=pxelinux.0,pxeserver,DHCP-SERVEUR
+pxe-service=x86PC, "Install Linux", pxelinux
+enable-tftp
+tftp-root=/srv/tftp
+log-queries
+log-facility=/var/log/dnsmasq.log
+" | sudo tee /etc/dnsmasq.d/pxe.conf
+
+sudo mkdir -p /srv/tftp
+curl https://deb.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/netboot.tar.gz | tar xzf -C /srv/tftp -
