6fe87b03a0fbc6557f98b07359628dfd3fe48eed
Ludovic <leo.le.lion@free.fr>
2025-10-19 01:46:47 +0300

    Opentofu: BPG/Proxmox upgrade to version 0.85

--- jenkins_VM/jenkins_agent/opentofu/provider.tf
+++ jenkins_VM/jenkins_agent/opentofu/provider.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.67.0"
+      version = "~> 0.85.0"
     }
   }
 }
 
 # https://registry.terraform.io/providers/bpg/proxmox/latest/docs
+
 provider "proxmox" {
   #  endpoint = "https://192.168.0.11:8006/"
   #  api_token = var.api_token
